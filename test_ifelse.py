33caf9d0588d38440fde1f7e2785e5ad6daf7b99
vishal rajput <vishalrajpoot540@gmail.com>
2020-10-18 16:16:43 +0530

    Update test_ifelse.py

--- test_ifelse.py
+++ test_ifelse.py
@@ -2,7 +2,7 @@ age=int(input("what is your age: "))
 if age>18 and age<100:
     print("you can drive")
 elif age==18:
-    print("we can't decide, you can come to our office then we check your mental health ")
+    print("we can't decide, you can come to our office then we will check your mental health ")
 elif age<18 and age>7:
     print("you can't drive")
 else:
